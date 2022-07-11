8c0c22b9e45ce154e45f8d8d12642a7a41be3d56
Nothung429 <frank.gatto96.fg@gmail.com>
2022-07-11 15:25:07 +0200

    fix

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,17 +9,13 @@ class MovieController extends Controller
 {
     public function index()
     {
-        // SELECT * FROM books
         $movies = Movie::all();
-
-        //['books' => $books]
+        
         return view('homepage', compact('movies'));
     }
 
     public function show($id)
     {
-        // SELECT * FROM books WHERE id = $id
-        // $book = Book::where('id', $id)->first();
         $movie = Movie::find($id);
 
         return view('movie', compact('movie'));
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'MovieController@index')->name('homepage');
-Route::get('/book/{id}', 'MovieController@show')->name('show');
+Route::get('/movie/{id}', 'MovieController@show')->name('show');
